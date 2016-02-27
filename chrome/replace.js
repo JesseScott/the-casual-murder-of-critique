c05d3e275315f046df91a1c663f6111584019502
Jesse Scott <jessecolinscott@gmail.com>
2016-02-26 16:56:30 -0800

    Catching Exceptions dur to array order

--- chrome/replace.js
+++ chrome/replace.js
@@ -1,20 +1,29 @@
 
-jQuery.fn.textWalk = function( fn ) {
+jQuery.fn.textWalk = function( fn )
+{
     this.contents().each( jwalk );
 
-    function jwalk() {
+    function jwalk()
+    {
         var nn = this.nodeName.toLowerCase();
-        if( nn === '#text') {
+        if( nn === '#text')
+        {
             fn.call( this );
-        } else if( this.nodeType === 1 && this.childNodes && this.childNodes[0] && nn !== 'script' && nn !== 'textarea' ) {
+        }
+        else if( this.nodeType === 1 && this.childNodes && this.childNodes[0] && nn !== 'script' && nn !== 'textarea' )
+        {
             $(this).contents().each( jwalk );
         }
     }
     return this;
 };
 
-$('body').textWalk(function() {
-    this.data = this.data.replace('Trump', '[THE CASUAL MUDER OF CRITIQUE]');
-    this.data = this.data.replace('The Donald', '[THE CASUAL MUDER OF CRITIQUE]');
-    this.data = this.data.replace('Donald Trump', '[THE CASUAL MUDER OF CRITIQUE]');
+$('body').textWalk(function()
+{
+    var msg = 'the casual murder of critique';
+    this.data = this.data.replace('Donald Trump', msg);
+    this.data = this.data.replace('Donald J. Trump', msg);
+    this.data = this.data.replace('The Donald', msg);
+    this.data = this.data.replace('Trump', msg);
+    this.data = this.data.replace('Mr Trump\'s', msg);
 });
